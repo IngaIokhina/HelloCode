064076c3c6943c1a258e2e41d3f017eb8e08698c
Inga Iokhina <iiokhina95@mail.ru>
2022-11-28 19:09:07 +0400

    add Example 005 Понять в чём ошибка

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿// НЕПОЛУЧИЛОСЬ, ПОНЯТЬ ПОЧЕМУ!!!
+
+Console.Write ("Enter username: ");
+string username = Console.ReadLine();
+if(username == "Nikita")
+{
+    Сonsole.WriteLine("Ura! Nikita");
+}
+else
+{
+    Сonsole.Write("Hi, ");
+    Сonsole.WriteLine(username);
+}
