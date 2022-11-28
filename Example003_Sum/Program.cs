0ec15e66abc77ca28d65111094200fadba98bec3
Inga Iokhina <iiokhina95@mail.ru>
2022-11-28 18:51:12 +0400

    add Example 003 SUM Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberВ = 5;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberВ = new Random().Next(1,10);
+Console.WriteLine(numberВ);
 Console.WriteLine(numberA + numberВ );
